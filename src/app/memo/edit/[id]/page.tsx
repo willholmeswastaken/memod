3f1314f9e7e0be38f66e13358df2252c69c5d856
Will Holmes <willholmes97@icloud.com>
2023-11-26 19:01:37 +0000

    more pages

---
+++ src/app/memo/edit/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function MemoEditPage() {
+    return (
+        <div>Hello world!</div>
+    )
+}
---
+++ src/app/memo/review/[id]/page.tsx
@@ -0,0 +1,5 @@
+export default function MemoReviewPage() {
+    return (
+        <div>Hello world!</div>
+    )
+}
