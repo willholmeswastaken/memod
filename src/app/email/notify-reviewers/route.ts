6f2e8c34f894786d5b5d64619d60a1983539268a
Will Holmes <willholmes97@icloud.com>
2023-12-05 19:25:52 +0000

    chore: Remove biome and tidy up unused deps

--- src/app/email/notify-reviewers/route.ts
+++ src/app/email/notify-reviewers/route.ts
@@ -6,21 +6,21 @@ import { EmailTemplate } from "../../../components/EmailTemplate";
 const resend = new Resend(env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
-	try {
-		const { participants, memo } = await request.json();
-		await Promise.all(
-			participants.map(
-				async (participant: { name: string; email: string }) =>
-					await resend.emails.send({
-						from: "Memod <memod@willdevsapps.com>",
-						to: [participant.email],
-						subject: "Memod: New Memo",
-						react: EmailTemplate({ name: participant.name, memo }),
-					}),
-			),
-		);
-		return new Response("done", { status: 200 });
-	} catch (error) {
-		return NextResponse.json({ error });
-	}
+  try {
+    const { participants, memo } = await request.json();
+    await Promise.all(
+      participants.map(
+        async (participant: { name: string; email: string }) =>
+          await resend.emails.send({
+            from: "Memod <memod@willdevsapps.com>",
+            to: [participant.email],
+            subject: "Memod: New Memo",
+            react: EmailTemplate({ name: participant.name, memo }),
+          }),
+      ),
+    );
+    return new Response("done", { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
 }
